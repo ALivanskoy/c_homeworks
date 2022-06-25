1f3733f9831ea49f7edc9bea61712517eb48852a
ALivanskoy <a.a.shutov@bk.ru>
2022-06-25 13:52:35 +0300

    ДЗ1 обязательные задачи

---
+++ homeworks_1/Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int num1, num2;
+
+// Кусок закомментированного кода в строках 11 - 14 выдаёт какую-то околесицу, в то время как код на строках 16 - 19 работает исправно. Не понимаю в чём дело
+
+// Console.WriteLine("Введите первое число: ");
+// num1 = Convert.ToInt32(Console.Read());
+// Console.WriteLine("Введите второе число: ");
+// num2 = Convert.ToInt32(Console.Read());
+
+Console.WriteLine("Введите число 1:");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.Write($"{num1} > {num2}");
+else if (num2 > num1)
+    Console.Write($"{num1} < {num2}");
+else if (num2 == num1)
+    Console.Write($"{num2} = {num1}");
+else
+    Console.Write("Произошла какая-то ошибка ");
---
+++ homeworks_1/Task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int[] num = new int[3];
+int max = 0;
+
+Console.WriteLine("Введите число 1:");
+num[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+num[1] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3:");
+num[2] = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < num.Length; i++)
+{
+    if (max < num[i]) max = num[i];
+}
+
+Console.WriteLine($"Максимальное из введёных чисел: {max}");
